8b027c2af88bfaed3ada6d6900ef155177175f3b
Septito <septito2k21@gmail.com>
2026-06-06 16:27:54 +0700

    Fix particles.js gak muncul

--- script.js
+++ script.js
@@ -199,7 +199,7 @@ document.addEventListener('DOMContentLoaded', () => {
     particlesJS('particles-js', {
       "particles": {
         "number": { "value": 60, "density": { "enable": true, "value_area": 800 } },
-        "color": { "value": ["#38bdf8", "#818cf8", "#c084fc"] },
+        "color": { "value": "#38bdf8" },
         "shape": { "type": "circle" },
         "opacity": { "value": 0.5, "random": true },
         "size": { "value": 3, "random": true },
